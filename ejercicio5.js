7dfb9867eecd241b4dd60d6409005fb400838fed
karlacabanas01 <karlacabanas01@gmail.com>
2023-04-20 16:04:37 -0400

    ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,8 @@
+// Cuanto es el X por ciento de X numero?
+function xForX (numero, porcentaje){
+    
+    let operacion = (numero * porcentaje) / 100
+    
+    console.log("Devuelve: " + operacion);
+}
+xForX(43, 897)
